7850299a0e426fe8fda7a4ee582a08804a300411
priyaraghunath <priyankabraghunath98@gmail.com>
2025-06-24 13:32:27 -0400

    to keep the pods alive Update App.java

--- app/myapp/src/main/java/com/example/App.java
+++ app/myapp/src/main/java/com/example/App.java
@@ -6,8 +6,11 @@ package com.example;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main(String[] args) throws Exception
     {
-        System.out.println( "Hello World!" );
+        System.out.println("Hello World from CICD pipeline!");
+        while (true) {
+            Thread.sleep(10000); 
+        }
     }
 }
