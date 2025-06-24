b53dd67abc78fd83f9a11e0ede56be0474b06dcf
priyaraghunath <priyankabraghunath98@gmail.com>
2025-06-24 15:40:27 -0400

    Update App.java

--- app/myapp/src/main/java/com/example/App.java
+++ app/myapp/src/main/java/com/example/App.java
@@ -1,16 +1,8 @@
-package com.example;
-
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main(String[] args) throws Exception
-    {
+public class App {
+    public static void main(String[] args) throws Exception {
         System.out.println("Hello World from CICD pipeline!");
         while (true) {
-            Thread.sleep(10000); 
+            Thread.sleep(10000); // keep alive
         }
     }
 }
